65047a4487cb419521e423d97efeefc451d2a90d
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-08-17 14:33:14 -0500

    attempting res.header

--- server/routes/contactRouter.js
+++ server/routes/contactRouter.js
@@ -13,22 +13,25 @@ const corsOptions = {
 }
 
 // enable cors preflight
-router.options('/', cors())
+router.options('*', cors());
 
-// POST route
-router.post('/', cors(corsOptions), async (req, res, next) => {
+router.use()
     console.log('initiate POST email');
     console.log('req.body = ', req.body);
     const reqBody = JSON.parse(req.body);
+    console.log('parsed reqBody = ', reqBody);
     const mailBody = {
         recipient: process.env.RECIPIENT,
         message: reqBody.message
     };
-    try {
+    try 
+    {
         await sendEmail(mailBody.recipient, mailBody.message);
         res.json({message: 'Your query has been sent',
         mailBody
         });
+        await res.header("Access-Control-Allow-Origin", "*");
+        await res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept)");
         await next();
     } catch (error) {
         res.json({error});
